b534ebad911118dfa0a781eba80c6c4a5503d276
Mounisha0114 <151064852+Mounisha0114@users.noreply.github.com>
2024-07-10 12:03:26 +0530

    Create twonumbers.java

---
+++ twonumbers.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main (String []args) {
+        int num1=30,num2=40;
+        if(num1==num2)
+            System.out.println("both are equal");
+        else if(num1>num2)
+            System.out.println(num1 + " " + "is greater");
+        else
+            System.out.println(num2 + " " + "is greater");    
+    }
+}
