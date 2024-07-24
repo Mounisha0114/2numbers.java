9a84419edecfbe4e07d1e5500e585709fff5fab1
Mounisha0114 <151064852+Mounisha0114@users.noreply.github.com>
2024-07-24 11:35:32 +0530

    Create Default.java

---
+++ Default.java
@@ -0,0 +1,16 @@
+public class demo {
+	static boolean Val1;
+	static double Val2;
+	static float Val3;
+	static int Val4;
+	static long Val5;
+	static String Val6;
+	public static void main(String args[]) {
+		System.out.println("Val 1 ="+ Val1);
+		System.out.println("Val 2 ="+ Val2);
+		System.out.println("Val 3 ="+ Val3);
+		System.out.println("Val 4 ="+ Val4);
+		System.out.println("Val 5 ="+ Val5);
+		System.out.println("Val 6 ="+ Val6);
+		}
+	}
